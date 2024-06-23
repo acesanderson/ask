0bc8ca61013563fc3f2754ca17d95e5facd4822b
Brian <brian.briananderson@gmail.com>
2024-06-23 17:18:43 -0500

    added an escalate function which brings in the manager -- gpt4o

--- ask.py
+++ ask.py
@@ -17,11 +17,11 @@ dir_path = os.path.dirname(os.path.realpath(__file__))
 # Construct the path to the .env file
 message_store_path = os.path.join(dir_path, 'message_store.pkl')
 
-preferred_model = "gpt-3.5-turbo-0125"  # cheaper
-# preferred_model = "gpt" # more expensive
+preferred_model = "gpt-3.5-turbo-0125"
 
-system_instructions = """
-You are a helpful IT admin. You are helping a new programmer.
+frontline_worker = """
+You are a helpful IT admin. You are a frontline worker at your company.
+You are helping a new programmer.
 They use Python and Linux. They are experienced with Python programming but don't know much about how to do the following:
 - package scripts into proper applications
 - use git and GitHub
@@ -38,11 +38,36 @@ If a code snippet is all that the user needs, just provide the code snippet.
 Here are details about the user's hardware, OS, and software:
 """.strip()
 
+manager = """
+You are now the manager of a team of IT admins who help people with computer problems.
+
+Your customers usually fit this description:
+
+They use Python and Linux. They are experienced with Python programming but don't know much about how to do the following:
+- package scripts into proper applications
+- use git and GitHub
+- set up a development environment
+- use things like Docker, virtual environments, or networking tools
+- write shell scripts
+- use a terminal effectively
+- the linux filesystem or basic linux commands (beyond ls, mkdir, cd, mv, etc.)
+
+One of your employees has been helping a user (me!) and getting nowhere.
+Take a deep look at the message history and do the following:
+- summarize the converstation in a sentence
+- write a short description of what you think is the underlying issue
+- leveraging your considerable expertise in this area, come up with a suggestion to solve my problem.
+Be aware that I am short on time and already frustrated.
+
+Here are details about my hardware, OS, and software:
+""".strip()
+
+system_instructions = frontline_worker
+
 def get_system_info():
 	os_info = platform.system() + " " + platform.release()
 	python_version = platform.python_version()
 	cpu_model = platform.processor()
-
 	if platform.system() == "Darwin":
 		memory_cmd = ['sysctl', 'hw.memsize']
 		gpu_cmd = ['system_profiler', 'SPDisplaysDataType']
@@ -99,6 +124,19 @@ def query(messages):
 	response = Model(preferred_model).chat(messages)
 	return response
 
+def escalate(message_store):
+	"""
+	User calls this when they're frustrated; it's like "speaking with the manager."
+	"""
+	global preferred_model
+	if preferred_model == "gpt-3.5-turbo-0125":
+		preferred_model = "gpt-4o"
+		print("You're now talking to the manager.")
+		message_store.append([time(), {"role": "system", "content": manager}])
+	else:
+		print("You're already talking to the manager.")
+	return message_store
+
 def get_messages(message_store: list[list]) -> list[dict]:
 	"""
 	Takes a message store object (which is a list of lists, each list has timestamp (0th element) + a message object (1st element)),
@@ -137,6 +175,9 @@ def load_message_store() -> list[list]:
 
 if __name__ == '__main__':
 	message_store: list[list] = load_message_store()	# what the admin sees
+	if message_store[-1][1]['role'] == "system":
+		if message_store[-1][1]['content'].startswith("You are now the manager"):
+			preferred_model = "gpt-4o"
 	if len(sys.argv) > 1:
 		if sys.argv[1] == "system_info":
 			print(get_system_info())
@@ -146,6 +187,13 @@ if __name__ == '__main__':
 			sys.exit(0)
 		if sys.argv[1] == "clear":						# to wipe memory
 			save_message_store([])
+			sys.exit()
+		if sys.argv[1] == "escalate":
+			message_store = escalate(message_store)
+			query_response = query(get_messages(message_store))
+			print(query_response)
+			message_store.append([time(), {'role': 'assistant', 'content': query_response}])
+			save_message_store(message_store)
 			sys.exit()
 		input_prompt = " ".join(sys.argv[1:])
 		message_store.append([time(), {'role': 'user', 'content': input_prompt}])
